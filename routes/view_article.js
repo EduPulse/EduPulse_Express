c6eb15e3cdade9b3e5785618762248478201837e
Chathura Wanniarachchi <wachathuraj@gmail.com>
2021-07-19 02:53:40 +0530

    Basic viewArticle API constructed

---
+++ routes/view_article.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const feed = require('../modules/feed');
+const Post = require('../models/post');
+
+var router = express.Router();
+
+router.post('/', function (req, res, next) {
+    try {
+        let postData = req.body;
+        let postID=postData._id.toString();
+        Post.find({_id:postID}).populate('author', 'name personalEmail role profilePicture').exec(function(err, posts) {
+            if(err) {
+                console.error(err);
+                res.sendStatus(500);
+            }
+            res.json(posts);
+        })
+    } catch (error) {
+        res.sendStatus(500)
+    }
+});
+
+
+module.exports = router;
