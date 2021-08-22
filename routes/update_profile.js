5f47eb86b918d6758952f777c5081af8f16903b9
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-08-22 19:45:07 +0530

    Update profile academic user

--- routes/update_profile.js
+++ routes/update_profile.js
@@ -7,6 +7,67 @@ router.post('/user', function (req, res, next) {
     try {
         let userData = req.body;
         let userID = userData._id.toString();
+        User.findOne({_id: userID}).populate('').exec(function(err, result) {
+            if(err) {
+                console.error(err);
+                res.sendStatus(500);
+            }
+            res.json(result);
+        })
+    } catch (error) {
+        res.sendStatus(500)
+    }
+});
+
+router.post('/userProfileUpdate', function(req, res) {
+    let userID = req.body.userID;
+
+    try {
+        console.log(req.body);
+        
+        // if (req.body.name)
+        //     User.findByIdAndUpdate({_id: userID}, { name: req.body.name });
+
+        // if (req.body.personalEmail)
+        //     User.findByIdAndUpdate({_id: userID}, {  personalEmail: req.body.personalEmail });
+
+        // if (req.body.bio)
+        //     User.findByIdAndUpdate({_id: userID}, { bio: req.body.bio })
+
+        // if (req.body.gender)
+        //     User.findByIdAndUpdate({_id: userID}, { gender: req.body.gender })
+
+        // if (req.body.bday) 
+        //     User.findByIdAndUpdate({_id: userID}, { birthday: req.body.bday })
+
+        User.findByIdAndUpdate({_id: userID}, 
+            {
+                name: req.body.name,
+                personalEmail: req.body.personalEmail,
+                //academicEmail: req.body.academicEmail,
+                // profilePicture: req.body.profilePicture,
+                bio: req.body.bio,
+                gender: req.body.gender,
+                birthday: req.body.bday,
+                // univeristy: req.body.univeristy.toString(),
+                // faculty: req.body.faculty.toString(),
+            },
+            function(err) {  
+                if (err) {  
+                    res.send(err);  
+                    return;  
+                }  
+                res.send({data:"Record has been Updated..!!"});  
+            });
+    } catch (error) {
+        res.sendStatus(500);
+    }
+});
+
+router.post('/social', function (req, res, next) {
+    try {
+        let userData = req.body;
+        let userID = userData._id.toString();
         User.findOne({_id:userID}).populate('').exec(function(err, result) {
             if(err) {
                 console.error(err);
