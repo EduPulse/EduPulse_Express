2889f06fcf3b5ea1383a7a8ea96f8f1615cf1d66
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-14 21:48:31 +0530

    Updated author_profile.js

--- routes/author_profile.js
+++ routes/author_profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const Report = require('../models/report')
+const Report = require('../models/report');
+const User = require('../models/user');
 
 var router = express.Router();
 
@@ -7,18 +8,53 @@ router.post('/report_author', async function (req, res, next) {
     try {
         console.log("Report req body: ", req.body);
         
-        //report count == 10 -> disabled
+        // const instance = new Report ({
+        //     reportedBy: req.body.reported_by,
+        //     type: req.body.report_type,
+        //     title: req.body.report_title,
+        //     message: req.body.report_reason,
+        //     against: {user: req.body.reported_author}
+        // });
 
-        const instance = new Report ({
-            reportedBy: req.body.reported_by,
-            type: req.body.report_type,
-            title: req.body.report_title,
-            message: req.body.report_reason,
-            against: {user: req.body.reported_author}
-            },
-        )
+        // await instance.save().then(res.send({data:"Report recorded in report model..!!"}));
 
-        await instance.save().then(res.send({data:"Report recorded in report model..!!"}));
+        await User.findOneAndUpdate(
+            {_id: req.body.reported_author},
+            {$push: {reports: [{
+                    reportedBy: req.body.reported_by,
+                    title: req.body.report_title,
+                    message: req.body.report_reason,
+                }]
+            }},
+        ).exec((
+            function(err, result) {
+                if (err) {  
+                    res.send(err);  
+                    return;  
+                }
+                res.json(result);
+                // const count = User.findById({_id: req.body.reported_author}).select('reports');
+                // console.log("Count: ", count.body);
+                // else {
+                //     const reportCount = []; 
+                //     reportCount = User.findById(req.body.reported_author).select('reports');
+                //     console.log("num of reports: ", reportCount)
+                //     if (reportCount == 10) {
+                //         User.findOneAndUpdate({_id: req.body.reported_author}, {
+                //             accountStatus: "onHold"
+                //         },
+                //         function(err) {
+                //             if (err) {
+                //                 res.send(err);  
+                //                 return;  
+                //             }
+                //             res.json(result);
+                //         }
+                //         )
+                //     }
+                // }
+            }
+        ))
     } catch (error) {
         res.sendStatus(500)
     }
