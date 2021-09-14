2fd9ea313f15e8d15e5ee41163fad7e749921a5c
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-14 06:45:30 +0530

    Updated Author_profile.js

--- routes/author_profile.js
+++ routes/author_profile.js
@@ -1,47 +1,24 @@
 const express = require('express');
-const User = require('../models/user');
 const Report = require('../models/report')
 
 var router = express.Router();
 
-router.post('/report_author', function (req, res, next) {
+router.post('/report_author', async function (req, res, next) {
     try {
-        console.log(req.body);
-        //  let reportedUser = req.body.reported_author;
-       
-        // User.findByIdAndUpdate({_id: reportedUser}, 
-        //     {
-        //         reports:[{
-        //             reportedBy: req.body.reported_by, 
-        //             title: req.body.report_title, 
-        //             message: req.body.report_reason
-        //         } ]
-        //     },
-        //     function(err) {  
-        //         if (err) {  
-        //             res.send(err);  
-        //             return;  
-        //         }  
-        //         res.send({data:"Report recorded in user model..!!"});  
-        //     }
-        // );
+        console.log("Report req body: ", req.body);
+        
+        //report count == 10 -> disabled
 
-        Report.create({
+        const instance = new Report ({
             reportedBy: req.body.reported_by,
             type: req.body.report_type,
             title: req.body.report_title,
             message: req.body.report_reason,
-            against: req.body.reported_author,
-            against: [{user: [{_id: req.body.reported_author}]}]
+            against: {user: req.body.reported_author}
             },
-            function(err) {  
-                if (err) {  
-                    res.send(err);  
-                    return;  
-                }  
-                res.send({data:"Report recorded in report model..!!"});  
-            }
         )
+
+        await instance.save().then(res.send({data:"Report recorded in report model..!!"}));
     } catch (error) {
         res.sendStatus(500)
     }
