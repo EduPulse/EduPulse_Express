fd1f36f14af04e4eb373c5dde3965c46e4d8048a
Mahela_jay <57103078+Mahelajayasinghe@users.noreply.github.com>
2021-09-17 22:48:07 +0530

    updated logout on log.js

--- modules/log.js
+++ modules/log.js
@@ -27,6 +27,10 @@ const userLogin = (verbose) => {
     save('LOGIN', verbose);
 };
 
+const userLogout = (verbose) => {
+    save('LOGOUT', verbose);
+};
+
 module.exports = {
     info,
     warn,
