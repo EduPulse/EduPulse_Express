4706594c947badfbb9e7f0dd1a2302822d04815e
Mahelajay <57103078+Mahelajayasinghe@users.noreply.github.com>
2021-09-18 15:50:34 +0530

    first build done

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ const passportConfig = require('./config/passport');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 //passport
 app.use(session({
   secret: config.sessionSecret,
@@ -33,6 +29,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 // open-id
 var openIDRouter = require('./routes/openID');
 app.use('/openid', openIDRouter);
@@ -41,11 +39,7 @@ app.use('/openid', openIDRouter);
 var APIRouter = require('./routes/API');
 app.use('/api', APIRouter);
 
-//api
-/* var APIRouter = require('./routes/API');
-app.use('/api', APIRouter); */
-
-// route handlers
+// ****** MOVE TO API.js route handlers
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
@@ -56,21 +50,10 @@ var authorProfile = require('./routes/get_authorProfile');
 app.use('/update_profile', updateProfile);
 app.use('/loggedIn_User', loggedInUser);
 app.use('/get_authorProfile', authorProfile);
+// ****************************
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500); 
-  res.render('error');
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = app;
